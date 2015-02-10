5f05b2cae7bd24881507b36a9bf7231816ca0718
Иван Бухов <ivanbuhov@gmail.com>
2015-02-10 15:57:06 +0200

    Revert commit 5ec9657c97a0e17dd9364306ea5b1724377b4744

--- gruntfile.js
+++ gruntfile.js
@@ -161,10 +161,6 @@ module.exports = function (grunt) {
             libJavaScriptCore_copyHeaders: {
                 cmd: "<%= srcDir %>/build/scripts/JSCCopyHeaders.sh"
             },
-            libJavaScriptCore_archive: {
-                cmd: "tar -cvzf jsc.tgz ./jsc",
-                cwd: "<%= outDistDir %>"
-            },
 
             libNativeScript_i386_x86_64: {
                 cmd: "xcodebuild -configuration Release -sdk iphonesimulator -scheme NativeScript -workspace <%= srcDir %>/src/NativeScript/NativeScript.xcworkspace SYMROOT=../../<%= outNativeScriptIntermediateDir %> ARCHS=\"i386 x86_64\" VALID_ARCHS=\"i386 x86_64\" clean build > <%= outBuildLog %>"
@@ -324,8 +320,7 @@ module.exports = function (grunt) {
         "exec:libJavaScriptCore_armv7_arm64",
         "exec:libJavaScriptCore_universal",
         "exec:libJavaScriptCore_copyHeaders",
-        "clean:outJscIntermediates",
-        "exec:libJavaScriptCore_archive"
+        "clean:outJscIntermediates"
     ]);
 
     grunt.registerTask("build", [
