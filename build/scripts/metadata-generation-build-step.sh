f92a3e8debaf5ca64fef926d9ab1b790f0004939
Иван Бухов <ivanbuhov@gmail.com>
2015-05-05 14:49:22 +0300

    Added escaping of paths in metadata generation script

--- build/scripts/metadata-generation-build-step.sh
+++ build/scripts/metadata-generation-build-step.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 function generateMetadata {
-    ./objc-metadata-generator -isysroot "$SDKROOT" -arch $1 -iphoneos-version-min $IPHONEOS_DEPLOYMENT_TARGET -target arm-apple-darwin -std $GCC_C_LANGUAGE_STANDARD -header-search-paths $HEADER_SEARCH_PATHS -framework-search-paths $FRAMEWORK_SEARCH_PATHS -output-bin "$BUILT_PRODUCTS_DIR/$CONTENTS_FOLDER_PATH/metadata-$1.bin" -output-umbrella "$BUILT_PRODUCTS_DIR/umbrella-$1.h" 2> $BUILT_PRODUCTS_DIR/metadata-generation-stderr-$1.txt
+    ./objc-metadata-generator -isysroot "$SDKROOT" -arch $1 -iphoneos-version-min $IPHONEOS_DEPLOYMENT_TARGET -target arm-apple-darwin -std $GCC_C_LANGUAGE_STANDARD -header-search-paths "$HEADER_SEARCH_PATHS" -framework-search-paths "$FRAMEWORK_SEARCH_PATHS" -output-bin "$BUILT_PRODUCTS_DIR/$CONTENTS_FOLDER_PATH/metadata-$1.bin" -output-umbrella "$BUILT_PRODUCTS_DIR/umbrella-$1.h" 2> "$BUILT_PRODUCTS_DIR/metadata-generation-stderr-$1.txt"
     if [ $? -ne 0 ]; then
-    cat $BUILT_PRODUCTS_DIR/metadata-generation-$1.txt
+    	cat "$BUILT_PRODUCTS_DIR/metadata-generation-$1.txt"
     fi
 }
 
