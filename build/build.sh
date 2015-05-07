6d50f82f9b361943f980b4d12c0e68d2b3091bad
Jason Zhekov <jasssonpet@gmail.com>
2015-05-07 18:08:51 +0300

    Fix build

--- build/build.sh
+++ build/build.sh
@@ -9,6 +9,25 @@ CMAKE_FLAGS="-G Xcode -DCMAKE_C_COMPILER_WORKS=YES -DCMAKE_CXX_COMPILER_WORKS=YE
 mkdir -p $WORKSPACE/cmake-build
 cd $WORKSPACE/cmake-build
 
+cmake .. -DCMAKE_OSX_SYSROOT=iphonesimulator $CMAKE_FLAGS -DBUILD_SHARED_LIBS=YES
+cmake --build . --config Release --target NativeScript
+
+cmake .. -DCMAKE_OSX_SYSROOT=iphoneos $CMAKE_FLAGS -DBUILD_SHARED_LIBS=YES
+cmake --build . --config Release --target NativeScript
+
+echo "Packaging NativeScript-Shared..."
+mkdir -p $WORKSPACE/dist/NativeScriptEmbedded/Release
+cp -r $WORKSPACE/cmake-build/src/NativeScript/Release-iphoneos/NativeScript.framework $WORKSPACE/dist/NativeScriptEmbedded/Release
+
+mkdir -p $WORKSPACE/dist/NativeScriptEmbedded/Debug
+cp -r $WORKSPACE/dist/NativeScriptEmbedded/Release/NativeScript.framework $WORKSPACE/dist/NativeScriptEmbedded/Debug
+rm $WORKSPACE/dist/NativeScriptEmbedded/Debug/NativeScript.framework/NativeScript
+lipo -create -output $WORKSPACE/dist/NativeScriptEmbedded/Debug/NativeScript.framework/NativeScript \
+    $WORKSPACE/cmake-build/src/NativeScript/Release-iphonesimulator/NativeScript.framework/NativeScript \
+    $WORKSPACE/cmake-build/src/NativeScript/Release-iphoneos/NativeScript.framework/NativeScript
+
+rm -rf *
+
 cmake .. -DCMAKE_OSX_SYSROOT=iphonesimulator $CMAKE_FLAGS
 cmake --build . --config Release --target NativeScript
 
@@ -30,20 +49,3 @@ cp \
     $NATIVESCRIPT_DIR/TNSRuntime.h \
     $NATIVESCRIPT_DIR/TNSRuntime+Inspector.h \
     $WORKSPACE/dist/NativeScript/include
-
-cmake .. -DCMAKE_OSX_SYSROOT=iphonesimulator $CMAKE_FLAGS -DBUILD_SHARED_LIBS=YES
-cmake --build . --config Release --target NativeScript
-
-cmake .. -DCMAKE_OSX_SYSROOT=iphoneos $CMAKE_FLAGS -DBUILD_SHARED_LIBS=YES
-cmake --build . --config Release --target NativeScript
-
-echo "Packaging NativeScript-Shared..."
-mkdir -p $WORKSPACE/dist/NativeScriptEmbedded/Release
-cp -r $WORKSPACE/cmake-build/src/NativeScript/Release-iphoneos/NativeScript.framework $WORKSPACE/dist/NativeScriptEmbedded/Release
-
-mkdir -p $WORKSPACE/dist/NativeScriptEmbedded/Debug
-cp -r $WORKSPACE/dist/NativeScriptEmbedded/Release/NativeScript.framework $WORKSPACE/dist/NativeScriptEmbedded/Debug
-rm $WORKSPACE/dist/NativeScriptEmbedded/Debug/NativeScript.framework/NativeScript
-lipo -create -output $WORKSPACE/dist/NativeScriptEmbedded/Debug/NativeScript.framework/NativeScript \
-    $WORKSPACE/cmake-build/src/NativeScript/Release-iphonesimulator/NativeScript.framework/NativeScript \
-    $WORKSPACE/cmake-build/src/NativeScript/Release-iphoneos/NativeScript.framework/NativeScript
