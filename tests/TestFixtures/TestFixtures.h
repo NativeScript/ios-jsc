6df4a60e832c99b441cd23344ad7c16f0fb00d78
Jason Zhekov <jasssonpet@gmail.com>
2015-09-11 15:29:48 +0300

    Add ambiguous metadata tests.

--- tests/TestFixtures/TestFixtures.h
+++ tests/TestFixtures/TestFixtures.h
@@ -9,21 +9,21 @@
 #import <UIKit/UIKit.h>
 
 #import "Api/TNSApi.h"
+#import "Api/TNSDeclarationConflicts.h"
 #import "Api/TNSVersions.h"
 
 #import "Functions/TNSFunctions.h"
 
-#import "Marshalling/TNSPrimitives.h"
-#import "Marshalling/TNSObjCTypes.h"
-#import "Marshalling/TNSRecords.h"
-#import "Marshalling/TNSPrimitivePointers.h"
-#import "Marshalling/TNSFunctionPointers.h"
-
-#import "Interfaces/TNSMethodCalls.h"
+#import "Interfaces/TNSClassWithPlaceholder.h"
 #import "Interfaces/TNSConstructorResolution.h"
 #import "Interfaces/TNSInheritance.h"
-#import "Interfaces/TNSClassWithPlaceholder.h"
+#import "Interfaces/TNSMethodCalls.h"
+
+#import "Marshalling/TNSFunctionPointers.h"
+#import "Marshalling/TNSObjCTypes.h"
+#import "Marshalling/TNSPrimitivePointers.h"
+#import "Marshalling/TNSPrimitives.h"
+#import "Marshalling/TNSRecords.h"
 
 #import "TNSTestCommon.h"
-
 #import "TNSTestNativeCallbacks.h"
