87c35251585b7fc36f4aae49983a69a541e50096
Jason Zhekov <jasssonpet@gmail.com>
2015-11-12 17:36:59 +0200

    Better error reporting

--- src/NativeScript/JSErrors.mm
+++ src/NativeScript/JSErrors.mm
@@ -8,8 +8,14 @@
 
 #include "JSErrors.h"
 
-#include "inspector/GlobalObjectInspectorController.h"
+#include <iostream>
+#include <iomanip>
+
 #include <JavaScriptCore/APICast.h>
+#include <JavaScriptCore/ScriptCallStack.h>
+#include <JavaScriptCore/ScriptCallStackFactory.h>
+#include "inspector/GlobalObjectInspectorController.h"
+#include "inspector/GlobalObjectConsoleClient.h"
 #import "TNSRuntime+Private.h"
 #import "TNSRuntime+Inspector.h"
 #import "TNSRuntime+Diagnostics.h"
@@ -53,15 +59,41 @@ void reportFatalErrorBeforeShutdown(ExecState* execState, Exception* exception) 
         uncaughtErrorHandler(toRef(execState), toRef(execState, exception->value()));
     }
 
-    globalObject->inspectorController().reportAPIException(execState, exception);
+    WTF::ASCIILiteral closingMessage("Fatal JavaScript exception - application has been terminated.");
 
     if (globalObject->debugger()) {
-        warn(execState, WTF::ASCIILiteral("Fatal exception - application has been terminated."));
+        warn(execState, closingMessage);
+
+        ASSERT(!globalObject->inspectorController().includesNativeCallStackWhenReportingExceptions());
+        globalObject->inspectorController().reportAPIException(execState, exception);
+
         while (true) {
             CFRunLoopRunInMode((CFStringRef)TNSInspectorRunLoopMode, 0.1, false);
         }
     } else {
-        WTFCrash();
+        std::cerr << "***** " << closingMessage << " *****\n";
+
+        std::cerr << "Native stack trace:\n";
+        WTFReportBacktrace();
+
+        std::cerr << "JavaScript stack trace:\n";
+        RefPtr<Inspector::ScriptCallStack> callStack = Inspector::createScriptCallStackFromException(execState, exception, Inspector::ScriptCallStack::maxCallStackSizeToCapture);
+        for (size_t i = 0; i < callStack->size(); ++i) {
+            Inspector::ScriptCallFrame frame = callStack->at(i);
+            std::cerr << std::setw(4) << std::left << std::setfill(' ') << (i + 1) << frame.functionName().utf8().data() << "@" << frame.sourceURL().utf8().data();
+            if (frame.lineNumber() && frame.columnNumber()) {
+                std::cerr << ":" << frame.lineNumber() << ":" << frame.columnNumber();
+            }
+            std::cerr << "\n";
+        }
+
+        std::cerr << "JavaScript error:\n";
+        // System logs are disabled in release app builds, but we want the error to be printed in crash logs
+        GlobalObjectConsoleClient::setLogToSystemConsole(true);
+        globalObject->inspectorController().reportAPIException(execState, exception);
+
+        *(int*)(uintptr_t)0xDEADDEAD = 0;
+        __builtin_trap();
     }
 }
 }
