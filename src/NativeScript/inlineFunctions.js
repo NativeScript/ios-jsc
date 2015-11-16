d13123910b583b1c3b51c647171ff8a2b412d756
Jason Zhekov <jasssonpet@gmail.com>
2015-11-16 12:06:14 +0200

    Refactor inline functions

--- src/NativeScript/inlineFunctions.js
+++ src/NativeScript/inlineFunctions.js
@@ -1,22 +1,20 @@
-function CGPointMake(x, y) {
-    return new CGPoint({ x: x, y: y });
-}
-
-function CGRectMake(x, y, width, height) {
-    return new CGRect({ origin: { x: x, y: y }, size: { width: width, height: height } });
-}
-
-function CGSizeMake(width, height) {
-    return new CGSize({ width: width, height: height });
-}
-
-function UIEdgeInsetsMake(top, left, bottom, right) {
-    return new UIEdgeInsets({ top: top, left: left, bottom: bottom, right: right });
-}
-
-function NSMakeRange(loc, len) {
-    return new NSRange({ location: loc, length: len });
-}
+Object.assign(global, {
+    CGPointMake(x, y) {
+        return new CGPoint({ x, y });
+    },
+    CGRectMake(x, y, width, height) {
+        return new CGRect({ origin: { x, y }, size: { width, height } });
+    },
+    CGSizeMake(width, height) {
+        return new CGSize({ width, height });
+    },
+    UIEdgeInsetsMake(top, left, bottom, right) {
+        return new UIEdgeInsets({ top, left, bottom, right });
+    },
+    NSMakeRange(location, length) {
+        return new NSRange({ location, length });
+    },
+});
 
 Object.defineProperty(global, "__tsEnum", {
     writable: false,
