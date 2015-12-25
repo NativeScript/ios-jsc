9aed6e2b4db9c7023e840a69b0a5bf0e0074c87c
Jason Zhekov <jasssonpet@gmail.com>
2015-12-25 12:36:02 +0200

    Allow vanilla TypeScript classes to extend null

--- src/NativeScript/__extends.js
+++ src/NativeScript/__extends.js
@@ -1,14 +1,8 @@
-(function __extends(Derived, Base) {
-    for (var key in Base) {
-        if (Base.hasOwnProperty(key)) {
-            Derived[key] = Base[key];
-        }
-    }
-
-    function __() {
-        this.constructor = Derived;
-    }
-
-    __.prototype = Base.prototype;
-    Derived.prototype = new __();
+// __extends is a TypeScript helper function generated when targeting older ES versions.
+// We inject our own function that branches logic whether base class is native or not.
+// This one gets called for classes that don't inherit from native ones.
+(function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 });
