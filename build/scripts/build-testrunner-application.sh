4d86288f4bd3bb26e109c688add6a6d2ca14af0b
ivanbuhov <ivanbuhov@gmail.com>
2017-07-11 16:57:41 +0300

    Enable producing .ipa for TestRunner app with Xcode 8.3

--- build/scripts/build-testrunner-application.sh
+++ build/scripts/build-testrunner-application.sh
@@ -14,13 +14,25 @@ mkdir -p "$WORKSPACE/cmake-build" && pushd "$_"
 # When using CMake 3.1 (which we have to) there is a bug that prevents building applications that link with a shared framework.
 rm -f "CMakeCache.txt"
 cmake .. -G"Xcode"
-xcodebuild -configuration "$CONFIGURATION" -sdk "iphoneos" -target "TestRunner" ARCHS="armv7 arm64" ONLY_ACTIVE_ARCH="NO" -quiet
+xcodebuild \
+-configuration "$CONFIGURATION" \
+-sdk "iphoneos" \
+-scheme "TestRunner" \
+-target "TestRunner" \
+ARCHS="armv7 arm64" \
+ONLY_ACTIVE_ARCH="NO" \
+-archivePath "$WORKSPACE/cmake-build/tests/TestRunner/$CONFIGURATION-iphoneos/TestRunner.xcarchive" \
+archive \
+-quiet
 popd
 
-checkpoint "Packaging TestRunner"
-xcrun -sdk "iphoneos" PackageApplication \
-    "$WORKSPACE/cmake-build/tests/TestRunner/$CONFIGURATION-iphoneos/TestRunner.app" \
-    -o "$WORKSPACE/cmake-build/tests/TestRunner/$CONFIGURATION-iphoneos/TestRunner.ipa"
+checkpoint "Exporting TestRunner"
+xcodebuild \
+-exportArchive \
+-archivePath "$WORKSPACE/cmake-build/tests/TestRunner/$CONFIGURATION-iphoneos/TestRunner.xcarchive" \
+-exportPath "$WORKSPACE/cmake-build/tests/TestRunner/$CONFIGURATION-iphoneos" \
+-exportOptionsPlist "$WORKSPACE/cmake/ExportOptions.plist"
+
 cp "$WORKSPACE/cmake-build/tests/TestRunner/$CONFIGURATION-iphoneos/TestRunner.ipa" "$DIST_DIR"
 
 checkpoint "Finished building TestRunner application - $DIST_DIR/TestRunner.ipa"
