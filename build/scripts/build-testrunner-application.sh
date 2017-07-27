21f97880d15a562b493cc8d5e3e4c0c638efdc62
ivanbuhov <ivanbuhov@gmail.com>
2017-07-27 10:01:53 +0300

    Enable producing .ipa for TestRunner app with Xcode 8.3

--- build/scripts/build-testrunner-application.sh
+++ build/scripts/build-testrunner-application.sh
@@ -14,16 +14,25 @@ mkdir -p "$WORKSPACE/cmake-build" && pushd "$_"
 # When using CMake 3.1 (which we have to) there is a bug that prevents building applications that link with a shared framework.
 rm -f "CMakeCache.txt"
 cmake .. -G"Xcode"
+
+# Define public TestRunner scheme. Xcode schemes is requried for archiving. This can be also done from Xcode GUI. However, CMake 3.1.3 has no support for generating Xcode schemes, so we have to do it manually.
+mkdir -p "$WORKSPACE/cmake-build/NativeScript.xcodeproj/xcshareddata/xcschemes"
+cp "$WORKSPACE/build/TestRunner.xcscheme" "$WORKSPACE/cmake-build/NativeScript.xcodeproj/xcshareddata/xcschemes/TestRunner.xcscheme"
+
 xcodebuild \
 -configuration "$CONFIGURATION" \
 -sdk "iphoneos" \
 -scheme "TestRunner" \
--target "TestRunner" \
 ARCHS="armv7 arm64" \
 ONLY_ACTIVE_ARCH="NO" \
+ -quiet
+
+xcodebuild archive \
 -archivePath "$WORKSPACE/cmake-build/tests/TestRunner/$CONFIGURATION-iphoneos/TestRunner.xcarchive" \
-archive \
--quiet
+-configuration "$CONFIGURATION" \
+-sdk "iphoneos" \
+-scheme "TestRunner" \
+ -quiet
 popd
 
 checkpoint "Exporting TestRunner"
