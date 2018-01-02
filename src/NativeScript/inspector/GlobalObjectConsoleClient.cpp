662286f72235b5b2c6362913316e21e4d9739e6a
Deyan Ginev <deyan.ginev@progress.com>
2018-01-02 14:17:56 +0200

    Refactor as per suggested ammendments.

--- src/NativeScript/inspector/GlobalObjectConsoleClient.cpp
+++ src/NativeScript/inspector/GlobalObjectConsoleClient.cpp
@@ -115,19 +115,17 @@ void GlobalObjectConsoleClient::takeHeapSnapshot(JSC::ExecState*, const String& 
 }
 
 void GlobalObjectConsoleClient::time(JSC::ExecState*, const String& title) {
-    std::unique_ptr<String> startMsg = m_consoleAgent->startTiming(title);
-    const String* url;
+    std::unique_ptr<Inspector::ConsoleMessage> startMsg = m_consoleAgent->startTiming(title);
     if (startMsg) {
-        ConsoleClient::printConsoleMessage(MessageSource::ConsoleAPI, MessageType::Timing, MessageLevel::Warning, *startMsg.get(), (const String&)url, -1, -1);
+        ConsoleClient::printConsoleMessage(startMsg->source(), startMsg->type(), startMsg->level(), startMsg->message(), startMsg->url(), startMsg->line(), startMsg->column());
     }
 }
 
 void GlobalObjectConsoleClient::timeEnd(JSC::ExecState* exec, const String& title) {
     Ref<Inspector::ScriptCallStack> callStack(Inspector::createScriptCallStackForConsole(exec, 1));
-    std::unique_ptr<String> stopMsg = m_consoleAgent->stopTiming(title, WTFMove(callStack));
-    const String* url;
+    std::unique_ptr<Inspector::ConsoleMessage> stopMsg = m_consoleAgent->stopTiming(title, WTFMove(callStack));
     if (stopMsg) {
-        ConsoleClient::printConsoleMessage(MessageSource::ConsoleAPI, MessageType::Timing, MessageLevel::Debug, *stopMsg.get(), (const String&)url, -1, -1);
+        ConsoleClient::printConsoleMessage(stopMsg->source(), stopMsg->type(), stopMsg->level(), stopMsg->message(), stopMsg->url(), stopMsg->line(), stopMsg->column());
     }
 }
 
