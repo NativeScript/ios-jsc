ad447ef078007ddd88d2a55070a972d9b0e5f035
Teodor Dermendzhiev <Teodor.Dermendzhiev@progress.com>
2018-06-19 16:51:47 +0300

    feat: Create NativeScriptEmbedder protocol
    When embedding a NativeScript application, the native class, calling the NS runtime, should conform to NativeScriptEmbedder protocol in order get a reference to the NativeScript application viewcontroller. Related to https://github.com/NativeScript/NativeScript/pull/5967

---
+++ src/NativeScript/NativeScriptEmbedder.h
@@ -0,0 +1,15 @@
+//
+//  NativeScriptEmbedder.h
+//  NativeScript
+//
+//  Created by Teodor Dermendzhiev on 6/19/18.
+//
+#include <UIKit/UIKit.h>
+
+//When embedding NativeScript application embedder needs to conform to this protocol
+//in order to have control over the NativeScript UIViewController
+//otherwise NativeScript application is presented over the topmost UIViewController.
+//https://github.com/NativeScript/NativeScript/pull/5967
+@protocol NativeScriptEmbedder
+- (id)presentNativeScriptApp:(UIViewController*)vc;
+@end
