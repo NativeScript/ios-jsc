c30787b79ae406955a140fea89e6dcf89c540142
Teodor Dermendzhiev <Teodor.Dermendzhiev@progress.com>
2018-06-20 13:13:05 +0300

    feat: Add modulemap file so NativeScriptEmbedder protocol is accessible from js
    Add modulemap to public headers so every included header data will be added to metadata

--- src/NativeScript/NativeScriptEmbedder.h
+++ src/NativeScript/NativeScriptEmbedder.h
@@ -6,10 +6,11 @@
 //
 #include <UIKit/UIKit.h>
 
-//When embedding NativeScript application embedder needs to conform to this protocol
-//in order to have control over the NativeScript UIViewController
-//otherwise NativeScript application is presented over the topmost UIViewController.
-//https://github.com/NativeScript/NativeScript/pull/5967
+// When embedding NativeScript application embedder needs to conform to this protocol
+// in order to have control over the NativeScript UIViewController
+// otherwise NativeScript application is presented over the topmost UIViewController.
+// Implemented in tns-core-modules by the following PR:
+// https://github.com/NativeScript/NativeScript/pull/5967
 @protocol NativeScriptEmbedder
 - (id)presentNativeScriptApp:(UIViewController*)vc;
 @end
