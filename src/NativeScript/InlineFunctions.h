d62afb5b58b88ddd3453601f96426b62a90fd42a
Teodor Dermendzhiev <Teodor.Dermendzhiev@progress.com>
2018-07-18 19:02:56 +0300

    WIP: Add inline functions .mm file and modulemap

---
+++ src/NativeScript/InlineFunctions.h
@@ -0,0 +1,11 @@
+//
+//  InlineFunctions.h
+//  NativeScript
+//
+//  Created by Teodor Dermendzhiev on 6/27/18.
+//
+
+#ifndef InlineFunctions_h
+#define InlineFunctions_h
+
+#endif /* InlineFunctions_h */
