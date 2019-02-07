17b4031d6cee2bb2268508dd6068775818e520bd
Martin Bektchiev <martin.bektchiev@progress.com>
2019-02-07 17:47:48 +0200

    fix(build): The correct option to use in cmake-gen.sh is -B

--- cmake-gen.sh
+++ cmake-gen.sh
@@ -1,5 +1,5 @@
 set -ex
-cmake -DBUILD_SHARED_LIBS=1 -GXcode . ---build cmake-build
+cmake -DBUILD_SHARED_LIBS=1 -GXcode . -Bcmake-build
 
 # Workaround for https://cmake.org/pipermail/cmake/2015-April/060484.html
 sed -i bak -e 's/Versions\/A\///g' cmake-build/NativeScript.xcodeproj/project.pbxproj
