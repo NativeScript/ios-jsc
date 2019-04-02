ef0a0c6559087f5b086cfc8407f96fecf6924ec0
Martin Bektchiev <martin.bektchiev@progress.com>
2019-04-02 17:34:54 +0300

    chore: Optimize TestRunner build
    Only do `xcodebuild` without performing the costy `xcodebuild archive`
    and `xcodebuild exportArchive`. Make a zip file with the `TestRunner.app`
    instead and call it `TestRunner.ipa`. We are only using this `.ipa` package
    to extract the `.app` and run it on a device with `ios-deploy` so it really
    doesn't need to be a "real" `.ipa`.

--- build/scripts/build-testrunner-application.sh
+++ build/scripts/build-testrunner-application.sh
@@ -9,11 +9,9 @@ CONFIGURATION=$NATIVESCRIPT_XCODE_CONFIGURATION
 
 checkpoint "Building TestRunner application"
 
-mkdir -p "$WORKSPACE/cmake-build" && pushd "$_"
-# Delete the CMake cache because a previous build could have generated the runtime with a shared framework.
-# When using CMake 3.1 (which we have to) there is a bug that prevents building applications that link with a shared framework.
-rm -f "CMakeCache.txt"
-cmake .. -G"Xcode"
+./cmake-gen.sh
+
+pushd "$WORKSPACE/cmake-build"
 
 # Define public TestRunner scheme. Xcode schemes is requried for archiving. This can be also done from Xcode GUI. However, CMake 3.1.3 has no support for generating Xcode schemes, so we have to do it manually.
 mkdir -p "$NATIVESCRIPT_XCODEPROJ/xcshareddata/xcschemes"
@@ -28,24 +26,17 @@ ONLY_ACTIVE_ARCH="NO" \
 -project $NATIVESCRIPT_XCODEPROJ \
 -quiet \
 
-xcodebuild archive \
--archivePath "$WORKSPACE/cmake-build/tests/TestRunner/$CONFIGURATION-iphoneos/TestRunner.xcarchive" \
--configuration "$CONFIGURATION" \
--sdk "iphoneos" \
--scheme "TestRunner" \
--project $NATIVESCRIPT_XCODEPROJ \
--quiet \
-
 popd
 
-checkpoint "Exporting TestRunner"
-xcodebuild \
--exportArchive \
--archivePath "$WORKSPACE/cmake-build/tests/TestRunner/$CONFIGURATION-iphoneos/TestRunner.xcarchive" \
--exportPath "$WORKSPACE/cmake-build/tests/TestRunner/$CONFIGURATION-iphoneos" \
--exportOptionsPlist "$WORKSPACE/cmake/ExportOptions.plist" \
--quiet \
+# Simulate an IPA by zipping TestRunner.app inside a `Payload` directory.
+# This way we're avoiding the costy steps of `xcodebuild archive` and `export`.
+(
+    set -e;
+    cd "$WORKSPACE/cmake-build/tests/TestRunner/$CONFIGURATION-iphoneos/";
+    mkdir Payload;
+    mv TestRunner.app Payload;
+    zip -r "$DIST_DIR/TestRunner.ipa" Payload;
+    mv Payload/TestRunner.app .
+)
 
-cp "$WORKSPACE/cmake-build/tests/TestRunner/$CONFIGURATION-iphoneos/TestRunner.ipa" "$DIST_DIR"
-
-checkpoint "Finished building TestRunner application - $DIST_DIR/TestRunner.ipa"
+checkpoint "Finished building TestRunner application - $DIST_DIR/TestRunner.app"
