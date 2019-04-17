ae86abf62ccfa84ca1739918b59a5762b9c106f0
Teodor Dermendzhiev <Teodor.Dermendzhiev@progress.com>
2019-04-17 14:24:37 +0300

    Support Swift files in the .xcodeproj *Redirect LD to nsld.sh so we can hook between compiling and linking and generate metadata there (we need <appname>-Swift.h to be created (and a modulemap for it) so the metadata-generator finds it) *Set Swift language version *Set Strip swift symbols to NO *Add ObjC bridging header so the Swift classes are added to the generated   <appname>-Swift.h

---
+++ build/project-template/internal/nsld.sh
@@ -0,0 +1,54 @@
+ #/bin/bash
+
+ set +o posix
+ set +e
+ source ./env_vars.sh
+ set -e
+ set +x
+
+ MODULES_DIR=$SRCROOT/internal/Swift-Modules
+
+ function DELETE_SWIFT_MODULES_DIR() {
+  rm -rf $MODULES_DIR
+}
+
+function GEN_MODULEMAP() {
+  DERIVED_SOURCES=$DERIVED_FILES_DIR
+
+  if [ -d "$DERIVED_SOURCES" ]; then
+    count=`ls $DERIVED_SOURCES/*-Swift.h 2>/dev/null | wc -l`
+
+      if [ $count == 1 ]
+      then
+          HEADER_PATH=`find $DERIVED_SOURCES -name *-Swift.h`
+          DELETE_SWIFT_MODULES_DIR
+
+          mkdir -p $MODULES_DIR
+          CONTENT="module nsswiftsupport { \n header \"$HEADER_PATH\" \n export * \n}"
+          printf "$CONTENT" > "$MODULES_DIR/module.modulemap"
+      else
+      echo "-Swift.h not found!"
+      fi
+  else
+    echo "Derived sources directory not found!"
+  fi
+
+}
+
+function GEN_METADATA() {
+  set -e
+
+  pushd "$SRCROOT/internal/metadata-generator/bin"
+  ./build-step-metadata-generator.py
+  popd
+}
+
+GEN_MODULEMAP
+printf "Generating metadata..."
+GEN_METADATA
+DELETE_SWIFT_MODULES_DIR
+$DT_TOOLCHAIN_DIR/usr/bin/clang $@
+
+
+
+
