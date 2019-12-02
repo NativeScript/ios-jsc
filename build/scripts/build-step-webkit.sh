468de10297af7bfeaff3fcc4147ebbf351961f68
Martin Bektchiev <martin.bektchiev@progress.com>
2019-12-02 12:22:53 +0200

    chore(build): Retry WebKit build when on Catalyst
    The new build system has some caching mechanism which checks
    in advance whether files to be compiled exist or not. It sporadically
    fails to find `DerivedSources/WTF/mach_excServer.c` when the
    generation is a bit slower on Jenkins slaves. As a workaround we're
    retrying the `xcodebuild` command. The second time around the file
    will have been generated in advance and no such racing condition will
    exist.

--- build/scripts/build-step-webkit.sh
+++ build/scripts/build-step-webkit.sh
@@ -3,19 +3,25 @@
 # This script is added as a run script phase in the CMake generated Xcode project.
 # Environment variables are provided by Xcode during build.
 # See the WebKit.cmake configuration file for more info.
-DESTINATION_ARG=""
-if [ $(echo $IS_UIKITFORMAC | tr '[:upper:]' '[:lower:]') = "yes" -o $(echo $IS_UIKITFORMAC | tr '[:upper:]' '[:lower:]') = "true" -o "$IS_UIKITFORMAC" = "1" ]; then
+
+if [ "$(echo $IS_UIKITFORMAC | tr '[:upper:]' '[:lower:]')" = "yes" -o "$(echo $IS_UIKITFORMAC | tr '[:upper:]' '[:lower:]')" = "true" -o "$IS_UIKITFORMAC" = "1" ]; then
     # Add compiler flags for "UIKit for Mac". See https://github.com/CocoaPods/CocoaPods/issues/8877#issuecomment-499752865
     # Taken from the output of `xcodebuild -scheme JavaScriptCore -showdestinations`
     DESTINATION_ARGS=(-destination "variant=UIKit for Mac,arch=x86_64" -UseModernBuildSystem=YES)
+    echo "info: Launching xcodebuild of JavaScriptCore with 1 retry attempt"
+    # retry once as a workaround of sporadic error: Build input file cannot be found: ...cmake-build/WebKit-prefix/src/WebKit-build/DerivedSources/WTF/mach_excServer.c' when using Xcode's new build system
+    set +e
 else
     DESTINATION_ARGS=(-sdk "$SDKROOT")
+    set -e
 fi
 
-xcodebuild build -scheme JavaScriptCore \
+for i in 1 2; do
+    xcodebuild build -scheme JavaScriptCore \
     "${DESTINATION_ARGS[@]}" \
     -configuration "$CONFIGURATION" \
     ARCHS="$ARCHS" \
     ONLY_ACTIVE_ARCH="$ONLY_ACTIVE_ARCH" \
     $DEPLOYMENT_TARGET_SETTING_NAME="${!DEPLOYMENT_TARGET_CLANG_ENV_NAME}" \
-    GCC_WARN_INHIBIT_ALL_WARNINGS="YES" \
+    GCC_WARN_INHIBIT_ALL_WARNINGS="YES"
+done
